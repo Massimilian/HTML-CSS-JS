94987db28069ddfd9547abd6c200f7f7d4bd6704
Vasily <vasalekmas@gmail.com>
2020-04-18 00:25:15 +0300

    Beonmax homework №2: 'Budget'

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,29 @@
-'use strict'
-
-let budget = prompt("Ваш бюджет на месяц?", "0");
-let date = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+let budget = +prompt("Ваш бюджет на месяц?", "0");
 
 let appData = {
-     bud: budget,
-     time: new Date(date),
-     expenses: {},
-     optionalExpences: {},
-     income: [],
-     savings: false,
+    bud: budget,
+    time: new Date(),
+    expenses: {},
+    optionalExpences: {},
+    income: [],
+    savings: false,
 };
 
-appData.expenses.article = prompt('Введите обязательную статью расходов в этом месяце', 'name');
-appData.expenses.sum = prompt('Во сколько обойдется?', 0);
+while (confirm('Есть ли обязательные статьи расходов?')) {
+    let article = prompt('Введите имя статьи расходов', 'name');
+    let sum = +prompt('Во сколько обойдется (в месяц)?', 0);
+    if (typeof (article) != null && typeof (sum) != null && article != '' && sum != '') {
+        appData.expenses[article] = sum;
+        alert("Статья расходов добавлена.");
+    } else {
+        alert("В указании статьи расходов допущена ошибка. Попробуйте ещё раз.");
+    }
+}
 
-let result = `Состояние бюджета на ${appData.time} - ${appData.bud}; обязательные траты на статью расходов ${appData.expenses.article} составляют ${appData.expenses.sum}; итоговый бюджет - ${appData.bud - appData.expenses.sum}.`;
-alert(result);
+let thisBudget = appData.bud;
+for (let key in appData.expenses) {
+    thisBudget -= appData.expenses[key];
+}
 
-
+appData.dayBud = Math.round(thisBudget / 30);
+alert("Свободный дневной бюджет = " + appData.dayBud);
